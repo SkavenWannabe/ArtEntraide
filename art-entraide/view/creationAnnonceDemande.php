c3ed38aab21d7b210b6ed834f362268ecdb35b54
Lucas Lacouture <lucas.lacouture1@etu.univ-grenoble-alpes.fr>
2020-11-05 00:12:01 +0100

    Le formulaire pour créer une annonce lorsque l'on recherche un service

---
+++ art-entraide/view/creationAnnonceDemande.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="/view/design/style.css">
+  </head>
+  <body>
+    <?php include_once(__DIR__."/../view/header.php"); ?>
+
+    <section>
+
+      <h2>Création d'une annonce</h2>
+
+      <form class="annonce" action="???" method="post">
+        <label for="intitule">Intitulé</label>
+        <input type="text" name="intitule" id="intitule">
+
+        <input list="categories" name="categorie" id="categorie">
+        <datalist id="categorie">
+          <option value="Jardinage">
+          <option value="Courses">
+          <option value="Bricolage">
+          <option value="Peinture">
+          <option value="Tuyauterie">
+          <option value="Animaux">
+          <option value="Baby-sitting">
+        </datalist>
+
+        <label for="description">Décrivez le service dont vous avez besoin</label>
+        <input type="text" name="date" id="date">
+
+        <label for="date">Quand avez-vous besoin d'aide ?</label>
+        <input type="date" name="date" id="date">
+
+        <button type="submit" name="publier">Publier l'annonce</button>
+      </form>
+    </section>
+
+    <?php include_once(__DIR__."/../view/footer.php"); ?>
+  </body>
+</html>
