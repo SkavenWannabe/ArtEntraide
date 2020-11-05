dcf7549ab89871c10a1fbea150b11d8a7deff9dd
Alexandre Legendre <alexandre.legendre@etu.univ-grenoble-alpes.fr>
2020-11-05 23:24:26 +0100

    "Optimisation" de la vue création annonce

---
+++ art-entraide/view/creationAnnonce.view.php
@@ -0,0 +1,48 @@
+<!-- Variables à donner à cette vue
+$categories : Liste du nom de chaque catégories
+$user : Utilisateur connecté
+ -->
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Création d'une annonce</title>
+    <link rel="stylesheet" href="/view/design/style.css">
+  </head>
+
+  
+  <body>
+    <?php include_once(__DIR__."/../view/header.php"); ?>
+
+    <section>
+
+      <h2>Créez votre propre annonce</h2>
+
+      <form class="creation_annonce" action="???" method="post">
+        <label for="intitule">Intitulé</label>
+        <input type="text" name="intitule" id="intitule">
+
+        <label for="categorie">Catégorie</label>
+        <select name="categorie" id="categorie">
+          <?php foreach ($categories as $key => $value) : ?>
+            <option value=""><?= $value ?></option>
+          <?php endforeach; ?>
+        </select>
+
+        <label for="description">Décrivez le service dont vous avez besoin</label>
+        <input type="text" name="description" id="description">
+
+        <label for="lieu">Lieu du service</label>
+        <input type="text" name="lieu" id="lieu" value="<?= $user->getAdresse() ?>">
+
+        <label for="date">Quand avez-vous besoin d'aide ?</label>
+        <input type="date" name="date" id="date">
+
+        <button type="submit" name="publier">Publier l'annonce</button>
+      </form>
+    </section>
+
+    <?php include_once(__DIR__."/../view/footer.php"); ?>
+  </body>
+</html>
