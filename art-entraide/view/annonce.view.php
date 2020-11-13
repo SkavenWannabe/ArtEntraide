3dd0f8b587676ba422bcc1310b8fec82d2a5cd17
Lucas Lacouture <lucas.lacouture1@etu.univ-grenoble-alpes.fr>
2020-11-13 13:32:48 +0100

    Update annonce.view.php

---
+++ art-entraide/view/annonce.view.php
@@ -0,0 +1,44 @@
+<!-- Variables à donner à cette vue
+$categories : Liste du nom de chaque catégories
+$user : Utilisateur connecté
+ -->
+ <?php
+
+   include_once(__DIR__."/../view/placeholderfunction.php");
+
+   if (isset($_GET['id'])){
+     $id = $_GET['id'];
+   } else {
+     $id = 0;
+   }
+
+   $titre = getAnnonceTitre($id);
+   $description = getAnnonceDescription($id);
+   $lieu = getAnnonceLieu($id);
+   $date = getAnnonceDate($id);
+
+  ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?= $titre ?></title>
+    <link rel="stylesheet" href="/view/design/style.css">
+  </head>
+
+  <body>
+    <?php include_once(__DIR__."/../view/header.php"); ?>
+
+    <section>
+
+      <h2><?= $titre ?></h2>
+
+      <p><?= $lieu ?></p>
+      <p><?= $date ?></p>
+      <p><?= $description ?></p>
+
+    </section>
+
+    <?php include_once(__DIR__."/../view/footer.php"); ?>
+  </body>
+</html>
