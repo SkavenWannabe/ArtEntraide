6203c15986f3c58e3f05f830deb63dca48e4a97e
Marion Chauvineau <marion.chauvineau@etu.univ-grenoble-alpes.fr>
2020-11-29 09:01:42 +0100

    Controleur pour annonce

--- art-entraide/controler/annonce.ctrl.php
+++ art-entraide/controler/annonce.ctrl.php
@@ -7,13 +7,26 @@ include_once(__DIR__."/../framework/view.class.php");
 //include_once(__DIR__."/../model/DAO.class.php");
 
 // ==== PARTIE RECUPERATION DES DONNEES ==== //
+$idAnnonce = $_GET['idAnnonce'];
+$idCategorie = $_GET['idCategorie'];
 
 // ==== PARTIE USAGE DU MODELE ==== //
+session_start();
+$art = new DAO();
+$annonce = $art->getAnnonce($idAnnonce);
+$nomAuteur = $annonce->getNom();
+$nomCategorie = $annonce->getIdCategorie()->getNom();
+
+session_write_close();
 
 // ==== PARTIE SELECTION DE LA VUE ==== //
 $view = new View();
-$view->assign('error',$error);
+//$view->assign('error',$error);
+//$view->display("annonce.view.php");
 
-$view->display("annonce.view.php");
+$view->assign('annonce', $annonce);
+$view->assign('nomAuteur',$nomAuteur);
+$view->assign('nomCategorie',$nomCategorie);
 
+$view->dispay("annonce.view.php");
 ?>
