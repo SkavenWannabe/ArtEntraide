1ec0860ed1228abf4cce1e783df066a119a0799a
carminav <vincenzo.carminati@etu.univ-grenoble-alpes.fr>
2020-12-04 15:48:19 +0100

    des inserts pour mettre en évidence les nulls.

--- art-entraide/data/insert.sql
+++ art-entraide/data/insert.sql
@@ -7,6 +7,7 @@ INSERT INTO Utilisateur VALUES(DEFAULT, 'jean.neymar@yahoo.fr', 'gonnaGoFast', '
 INSERT INTO Utilisateur VALUES(DEFAULT, 'bob.more@yahoo.fr', 'anne', 'Bob', 'Maure', 'Faceauvent', FALSE, 0);
 INSERT INTO Utilisateur VALUES(DEFAULT, 'tu.plante@yahoo.fr', '', '', '', '', TRUE, -101);
 INSERT INTO Utilisateur VALUES(DEFAULT, 'tu.plante@yahoo.fr', 'aussi', '', '', '', TRUE, 101);
+INSERT INTO Utilisateur VALUES(DEFAULT, 'celine.michel@yahoo.fr', 'celine.michel', 'Michel', 'Celine', NULL, DEFAULT, DEFAULT);
 
 INSERT INTO Categorie VALUES(DEFAULT, 'Baby-sitting');
 INSERT INTO Categorie VALUES(DEFAULT, 'Location');
@@ -17,6 +18,7 @@ INSERT INTO Annonce VALUES(DEFAULT, 'Recherche une personne pour garder notre en
 INSERT INTO Annonce VALUES(DEFAULT, 'Location camionnette', 'Je possède une camionnette qui ne me sert pas, je peux donc vous la prêter en échange d''un service', 'Grenoble', DEFAULT,DEFAULT,'11/11/2020', NULL, 2, 2);
 INSERT INTO Annonce VALUES(DEFAULT, 'Professeur particulier', 'Dans le cadre de ma formation d''enseignant, je cherche des étudiants à aider pour donner un plus à mon CV. Je peux aider du collège au Lycée.', 'Grenoble', DEFAULT,DEFAULT,'13/11/2020', NULL, 5, 3);
 INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage régulier', 'Je recherche quelqu''un avec le permis pour faire du covoiturage régulierement de Chambéry à Grenoble','Chambéry', DEFAULT,DEFAULT,'10/11/2020', NULL, 5, 4);
+INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage', NULL, NULL, DEFAULT, DEFAULT, '', NULL, 1, 1);
 
 INSERT INTO Message VALUES(DEFAULT, 'Bonjour, mon enfant est actuellement en 3ème. Êtes-vous capable de l''aider en histoire ?', '12/11/2020', 2);
 INSERT INTO Message VALUES(DEFAULT, 'Bonjour, je peux aider votre enfant en histoire. Je suis disponible le Mercredi après-midi et le Samedi toute la journée.', '13/11/2020', 5);
