d39fa8e90970ee33eebc793461889dbc504cb215
Synntix <alexandre.legendre38@gmail.com>
2021-01-07 15:56:46 +0100

    Style et required dans les formulaires

--- art-entraide/view/modifCompte.view.php
+++ art-entraide/view/modifCompte.view.php
@@ -16,23 +16,23 @@
       <h2>Modifier votre compte</h2>
 
       <form class="inscription" action="modifCompte.ctrl.php" method="post">
-        <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" value="<?= $user->getNom() ?>">
+        <label for="nom">Nom <em>*</em></label>
+        <input type="text" name="nom" id="nom" value="<?= $user->getNom() ?>" required>
 
-        <label for="prenom">Prénom</label>
-        <input type="text" name="prenom" id="prenom" value="<?= $user->getPrenom() ?>">
+        <label for="prenom">Prénom <em>*</em></label>
+        <input type="text" name="prenom" id="prenom" value="<?= $user->getPrenom() ?>" required>
 
         <label for="p_adresse">Adresse postale</label>
         <input type="text" name="p_adresse" id="p_adresse" value="<?= $user->getAdresse() ?>">
 
-        <label for="email">Adresse email</label>
-        <input type="email" name="email" id="email" value="<?= $user->getEmail() ?>">
+        <label for="email">Adresse email <em>*</em></label>
+        <input type="email" name="email" id="email" value="<?= $user->getEmail() ?>" required>
 
-        <label for="passwd">Mot de passe</label>
-        <input type="password" name="passwd" id="passwd">
+        <label for="passwd">Mot de passe <em>*</em></label>
+        <input type="password" name="passwd" id="passwd" required>
 
-        <label for="passwd">Confirmation du mot de passe</label>
-        <input type="password" name="passwdverif" id="passwdverif">
+        <label for="passwd">Confirmation du mot de passe <em>*</em></label>
+        <input type="password" name="passwdverif" id="passwdverif" required>
 
         <button type="submit" name="valider">Valider</button>
       </form>
