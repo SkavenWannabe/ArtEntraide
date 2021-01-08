09c0a110ce92d5d6272f2a4cb1e69f2b273f000a
sammiere <eliott.sammier@free.fr>
2021-01-08 18:07:51 +0100

    /!\ BDD A ACTUALISER - Ajout quelques catégories

--- art-entraide/data/insert.sql
+++ art-entraide/data/insert.sql
@@ -11,14 +11,16 @@ INSERT INTO Utilisateur VALUES(DEFAULT, 'celine.michel@yahoo.fr', 'celine.michel
 
 INSERT INTO Categorie VALUES(DEFAULT, 'Baby-sitting');
 INSERT INTO Categorie VALUES(DEFAULT, 'Location');
-INSERT INTO Categorie VALUES(DEFAULT, 'Aide au devoir');
+INSERT INTO Categorie VALUES(DEFAULT, 'Aide aux devoirs');
+INSERT INTO Categorie VALUES(DEFAULT, 'Bricolage');
+INSERT INTO Categorie VALUES(DEFAULT, 'Covoiturage');
 INSERT INTO Categorie VALUES(DEFAULT, 'Autre');
 
 INSERT INTO Annonce VALUES(DEFAULT, 'Recherche une personne pour garder notre enfant', 'Nous avons une réunion ce jour là. Nous avons donc besoin de quelqu''un pour garder notre enfant toute la journée.', 'Grenoble', DEFAULT,DEFAULT,'10/11/2020', NULL, 4, 1);
 INSERT INTO Annonce VALUES(DEFAULT, 'Location camionnette', 'Je possède une camionnette qui ne me sert pas, je peux donc vous la prêter en échange d''un service', 'Grenoble', DEFAULT,DEFAULT,'11/11/2020', NULL, 2, 2);
 INSERT INTO Annonce VALUES(DEFAULT, 'Professeur particulier', 'Dans le cadre de ma formation d''enseignant, je cherche des étudiants à aider pour donner un plus à mon CV. Je peux aider du collège au Lycée.', 'Grenoble', DEFAULT,DEFAULT,'13/11/2020', NULL, 5, 3);
-INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage régulier', 'Je recherche quelqu''un avec le permis pour faire du covoiturage régulierement de Chambéry à Grenoble','Chambéry', DEFAULT,DEFAULT,'10/11/2020', NULL, 5, 4);
-INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage', NULL, NULL, DEFAULT, DEFAULT, '10/10/2020', NULL, 1, 1);
+INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage régulier', 'Je recherche quelqu''un avec le permis pour faire du covoiturage régulierement de Chambéry à Grenoble','Chambéry', DEFAULT,DEFAULT,'10/11/2020', NULL, 5, 5);
+INSERT INTO Annonce VALUES(DEFAULT, 'Recherche covoiturage', NULL, NULL, DEFAULT, DEFAULT, '10/10/2020', NULL, 1, 5);
 
 INSERT INTO Message VALUES(DEFAULT, 'Bonjour, mon enfant est actuellement en 3ème. Êtes-vous capable de l''aider en histoire ?', '12/11/2020', 2);
 INSERT INTO Message VALUES(DEFAULT, 'Bonjour, je peux aider votre enfant en histoire. Je suis disponible le Mercredi après-midi et le Samedi toute la journée.', '13/11/2020', 5);
