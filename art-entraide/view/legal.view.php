99caf0e2da25e0e43ae0f43f60f0085a33ab520d
Lucas Lacouture <lucas.lacouture1@etu.univ-grenoble-alpes.fr>
2021-01-08 12:20:24 +0100

    J'avais confondu la page legal et confidentialité, c'est corrigé

--- art-entraide/view/legal.view.php
+++ art-entraide/view/legal.view.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content=" initial-scale=1, width=device-width "/>
-    <title>Nous contacter</title>
+    <title>Mentions légales</title>
     <link rel="stylesheet" href="/view/css/master.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
   </head>
@@ -16,10 +16,9 @@
 
 
     <section>
-      <h1>Politique de confidentialité</h1>
+    
+      <h1>Mentions légales</h1>
       <p>
-      On stocke vos données sur notre serveur mais on est pas des experts en sécurité donc voila :/<br>
-
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
 
