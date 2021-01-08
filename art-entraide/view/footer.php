edced8de281ba6badc215c721c1c1c0559c8f1c4
Lucas Lacouture <lucas.lacouture1@etu.univ-grenoble-alpes.fr>
2021-01-08 18:03:45 +0100

    Les logos fonctionnent bien maintenant (normalement)

--- art-entraide/view/footer.php
+++ art-entraide/view/footer.php
@@ -22,12 +22,12 @@
    </a>
 
     <a class="icon" href="https://www.facebook.com/">
-     <img alt="Facebook" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Ff%2Fff%2FFacebook_logo_36x36.svg%2F1200px-Facebook_logo_36x36.svg.png&f=1&nofb=1"
+     <img alt="Facebook" src="/view/design/Réseaux sociaux logos/f_logo_RGB-Blue_1024.png"
      width="30" height="30">
    </a>
 
     <a class="icon" href="https://www.instagram.com/">
-     <img alt="Instagram" src="/view/design/Réseaux sociaux logos/f_logo_RGB-Blue_1024.png"
+     <img alt="Instagram" src="/view/design/Réseaux sociaux logos/external-content.duckduckgo.com.png"
      width="30" height="30">
    </a>
 
