7ea087944a80584b06e7a912583c7137331b1e44
Lucas Lacouture <lucas.lacouture1@etu.univ-grenoble-alpes.fr>
2021-01-11 02:22:47 +0100

    Redesign du footer avec les lignes et le texte centré et tout

--- art-entraide/view/footer.php
+++ art-entraide/view/footer.php
@@ -1,37 +1,58 @@
 <footer>
+  <!--
   <header>
     <p>L'art de l'entraide</p>
     <a href="#"><img src="/view/design/Up-Arrow.svg" alt="">Haut de page</a>
   </header>
+-->
 
 <section>
-  <section>
-    <a href="/controler/pagesinfos.ctrl.php?page=description">Qui sommes-nous ?</a>
-    <a href="/controler/pagesinfos.ctrl.php?page=contact">Nous contacter</a>
-    <a href="/controler/pagesinfos.ctrl.php?page=FAQ">F.A.Q</a>
-    <a href="/controler/pagesinfos.ctrl.php?page=legal">Mentions légales</a>
-    <a href="/controler/pagesinfos.ctrl.php?page=confidentialite">Politique de confidentialité</a>
-  </section>
 
-  <section>
-    <h3>Suivez nous</h3>
+  <div class="hautdepage">
 
-    <a class="icon" href="https://twitter.com/">
-     <img alt="Twitter" src="/view/design/Réseaux sociaux logos/Twitter_Logo_Blue.svg"
-     width="35" height="30">
-   </a>
+    <a href="#"><img src="/view/design/Up-Arrow.svg" alt="">Haut de page</a>
 
-    <a class="icon" href="https://www.facebook.com/">
-     <img alt="Facebook" src="/view/design/Réseaux sociaux logos/f_logo_RGB-Blue_1024.png"
-     width="30" height="30">
-   </a>
+  </div>
 
-    <a class="icon" href="https://www.instagram.com/">
-     <img alt="Instagram" src="/view/design/Réseaux sociaux logos/external-content.duckduckgo.com.png"
-     width="30" height="30">
-   </a>
+  <div class="mainfooter">
 
-  </section>
+    <div>
+      <h3>A propos</h3>
+      <div class="text">
+        <a href="/controler/pagesinfos.ctrl.php?page=description">Qui sommes-nous ?</a>
+        <a href="/controler/pagesinfos.ctrl.php?page=contact">Nous contacter</a>
+        <a href="/controler/pagesinfos.ctrl.php?page=FAQ">F.A.Q</a>
+      </div>
+    </div>
+
+    <div>
+      <h3>Informations légales</h3>
+      <div class="text">
+        <a href="/controler/pagesinfos.ctrl.php?page=legal">Mentions légales</a>
+        <a href="/controler/pagesinfos.ctrl.php?page=confidentialite">Politique de confidentialité</a>
+      </div>
+    </div>
+
+    <div>
+      <h3>Suivez nous</h3>
+      <div class="reseaux">
+        <a class="icon" href="https://twitter.com/">
+          <img alt="Twitter" src="/view/design/Réseaux sociaux logos/Twitter_Logo_Blue.svg"
+           width="35" height="30">
+        </a>
+        <a class="icon" href="https://www.facebook.com/">
+         <img alt="Facebook" src="/view/design/Réseaux sociaux logos/f_logo_RGB-Blue_1024.png"
+         width="30" height="30">
+        </a>
+        <a class="icon" href="https://www.instagram.com/">
+         <img alt="Instagram" src="/view/design/Réseaux sociaux logos/external-content.duckduckgo.com.png"
+         width="30" height="30">
+        </a>
+      </div>
+    </div>
+
+  </div>
+
 </section>
 
 </footer>
