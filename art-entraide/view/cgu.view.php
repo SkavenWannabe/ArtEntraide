ca14ff1ad0d7214bd98bb0ed2db32168aa47ee95
Elian Loraux <elian.loraux@etu.univ-grenoble-alpes.fr>
2021-01-14 16:33:51 +0100

    rectification du titre

--- art-entraide/view/cgu.view.php
+++ art-entraide/view/cgu.view.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content=" initial-scale=1, width=device-width "/>
-    <title>Politique de confidentialité</title>
+    <title>Condition général d'utilisation</title>
     <link rel="stylesheet" href="/view/css/master.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
   </head>
